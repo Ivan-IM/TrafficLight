3d95931411914ec60dcf86b3ff2cdd54e31d8aeb
iMarishin <move_up@icloud.com>
2021-07-27 08:04:43 +0500

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Иван Маришин on 27.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
